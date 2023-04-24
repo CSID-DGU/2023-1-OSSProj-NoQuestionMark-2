1d2d9f4c3645b707902da789dce78147daae0137
bgs-study <qkrwlgp235@gmail.com>
2023-04-24 14:05:57 +0900

    feat(GlobalStyles) : globalStyels 적용

--- Front-end/schedular/src/App.tsx
+++ Front-end/schedular/src/App.tsx
@@ -1,9 +1,11 @@
 import AppRouter from './AppRouter';
 import './App.css';
+import GlobalStyle from './Styles/GlobalStyles';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <h1>Dongguk University E-class</h1>
       <AppRouter />
     </div>
