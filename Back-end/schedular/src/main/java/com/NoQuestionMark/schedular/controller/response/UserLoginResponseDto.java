db784ce4bc2a4109b5656b48fc7003f44308f4f0
shoeone96 <dlwnddnjs96@gmail.com>
2023-04-30 17:27:47 +0900

    feat(response): login 후 token 반환하는 dto 작성

---
+++ Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/controller/response/UserLoginResponseDto.java
@@ -0,0 +1,10 @@
+package com.NoQuestionMark.schedular.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserLoginResponseDto {
+    private String token;
+}
