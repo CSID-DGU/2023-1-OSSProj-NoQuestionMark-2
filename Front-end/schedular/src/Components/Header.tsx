7e8d1df37799b1bc24d0c6dd5d8e9f7cacd07a35
EunJeongmin <dmswjdals12@naver.com>
2023-04-30 22:24:48 +0900

    feat(Header): Header 파일 추가

---
+++ Front-end/schedular/src/Components/Header.tsx
@@ -0,0 +1,54 @@
+import SignIn from "../SignIn";
+import styled from "styled-components";
+import eclass_logo from "../Images/eclass_logo.png"
+
+
+      
+const Header = () => {
+  return(
+    <HeaderWapper>
+        <EclassLogo src={eclass_logo} alt="eclass_logo"/>
+        <SignIn></SignIn>
+        <a href="/SignUp">
+            <SignUpButton>회원가입</SignUpButton>
+        </a>
+      </HeaderWapper>
+  )
+}
+      
+const HeaderWapper = styled.div`
+  height: 100px;
+  display: flex;
+`;
+
+const EclassLogo = styled.img`
+  position: relative;
+  top: 0;
+  botton: 0;
+  margin: auto 10px auto 20px;
+  height: 42px;
+  width: 240px;  
+`;
+
+
+const SignUpButton = styled.button`
+  margin 33px 0;
+  height: 35px;
+  width: 80px;
+  text-align: center;
+  color: #fff;
+  padding: 7px 16px;
+  border: none;
+  border-radius: 2px;
+  background-color: #666;
+  font-size: 12px;
+`;
+
+const BgImage = styled.div`
+  height: 100%;
+  background-color: orange;
+  background-image: url(../Images/IMG_4174.jpeg);
+`
+
+
+export default Header;
