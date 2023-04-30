3258021f43225f11ba4f655617b143565c7ffc64
shoeone96 <dlwnddnjs96@gmail.com>
2023-04-30 17:03:53 +0900

    feature(exception): 로그인 관련 토큰 전달 사항 및 로그인 시 발생하는 예외 추가

--- Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/exception/ErrorCode.java
+++ Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/exception/ErrorCode.java
@@ -7,7 +7,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    DUPLICATED_CODE(HttpStatus.CONFLICT, "이미 존재하는 학번 정보입니다.");
+    DUPLICATED_CODE(HttpStatus.CONFLICT, "이미 존재하는 학번 정보입니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "찾으시는 유저가 없습니다."),
+    INVALID_PASSWORD(HttpStatus.CONFLICT, "비밀번호가 일치하지 않습니다.");
     private HttpStatus status;
     private String message;
 }
