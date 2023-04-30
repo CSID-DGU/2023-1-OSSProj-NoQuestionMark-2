92a434640b6a5ba4770a9b283bdb375c5d8f5c9f
shoeone96 <dlwnddnjs96@gmail.com>
2023-04-30 17:28:47 +0900

    feat(controller): login을 위한 controller 메소드  작성

--- Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/controller/UserController.java
+++ Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.NoQuestionMark.schedular.controller;
 
 import com.NoQuestionMark.schedular.controller.request.UserJoinRequestDto;
+import com.NoQuestionMark.schedular.controller.request.UserLoginRequestDto;
 import com.NoQuestionMark.schedular.controller.response.Response;
 import com.NoQuestionMark.schedular.controller.response.UserJoinResponseDto;
+import com.NoQuestionMark.schedular.controller.response.UserLoginResponseDto;
 import com.NoQuestionMark.schedular.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +20,12 @@ public class UserController {
     @PostMapping("/signin")
     public Response<UserJoinResponseDto> join(@RequestBody UserJoinRequestDto requestDto){
         return Response.success(userService.join(requestDto));
+    }
+
+    @PostMapping("/login")
+    public Response<UserLoginResponseDto> login(@RequestBody UserLoginRequestDto requestDto){
+        String token = userService.login(requestDto.getSchoolNumber(), requestDto.getPassword());
+        return Response.success(new UserLoginResponseDto(token));
     }
 
 }
