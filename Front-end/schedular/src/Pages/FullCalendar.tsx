d17d5c23340a9558d16eb38769fe0bb5f5d15164
bgs-study <qkrwlgp235@gmail.com>
2023-05-02 15:09:06 +0900

    styles(FullCalendar) : styled componet적용,export 변경

--- Front-end/schedular/src/Pages/FullCalendar.tsx
+++ Front-end/schedular/src/Pages/FullCalendar.tsx
@@ -1,19 +1,27 @@
 import React from 'react'
+import styled from 'styled-components';
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
-export default class DemoApp extends React.Component {
+const Container = styled.div`
+  width : 80%;
+  margin : 3rem auto;
+`;
+
+export default class Calendar extends React.Component {
   render() {
     return (
-      <FullCalendar
-        plugins={[ dayGridPlugin ]}
-        initialView="dayGridMonth"
-        weekends={false}
-        events={[
-          { title: 'event 1', date: '2023-05-01' },
-          { title: 'event 2', date: '2023-05-02' }
-        ]}
-      />
+      <Container>
+        <FullCalendar
+          plugins={[ dayGridPlugin ]}
+          initialView="dayGridMonth"
+          weekends={false}
+          events={[
+            { title: 'event 1', date: '2023-05-01' },
+            { title: 'event 2', date: '2023-05-02' }
+          ]}
+        />
+      </Container>
     )
   }
 }
