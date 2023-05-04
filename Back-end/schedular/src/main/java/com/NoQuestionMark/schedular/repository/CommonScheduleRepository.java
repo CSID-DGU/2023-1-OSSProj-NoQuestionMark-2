b905607acdfcedc27d55f1cd93c1db0a2db5e542
shoeone96 <dlwnddnjs96@gmail.com>
2023-05-05 01:33:24 +0900

    feat(repository): 글 저장을 위한 repository 작성

---
+++ Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/repository/CommonScheduleRepository.java
@@ -0,0 +1,8 @@
+package com.NoQuestionMark.schedular.repository;
+
+import com.NoQuestionMark.schedular.model.entity.CommonScheduleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommonScheduleRepository extends JpaRepository<CommonScheduleEntity, Long> {
+
+}
