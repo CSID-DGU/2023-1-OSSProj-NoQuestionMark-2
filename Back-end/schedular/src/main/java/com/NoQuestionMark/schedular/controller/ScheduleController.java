b92faa5bf7eb11f6fed753625585f66d2b6cb23f
shoeone96 <dlwnddnjs96@gmail.com>
2023-05-05 21:41:31 +0900

    feat(controller): 스케쥴을 받아오기 위한 controller 작성

--- Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/controller/ScheduleController.java
+++ Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/controller/ScheduleController.java
@@ -2,14 +2,14 @@ package com.NoQuestionMark.schedular.controller;
 
 
 import com.NoQuestionMark.schedular.controller.request.CommonScheduleRequestDto;
+import com.NoQuestionMark.schedular.controller.response.CommonScheduleResponseDto;
 import com.NoQuestionMark.schedular.controller.response.Response;
 import com.NoQuestionMark.schedular.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/schedule")
@@ -21,5 +21,10 @@ public class ScheduleController {
     public Response<Void> writeSchedule(@RequestBody CommonScheduleRequestDto requestDto, Authentication authentication){
         scheduleService.writeCommonSchedule(requestDto, authentication.getName());
         return Response.success();
+    }
+
+    @GetMapping("/common")
+    public Response<List<CommonScheduleResponseDto>> getSchedule(Authentication authentication){
+        return Response.success(scheduleService.getAllSchedule(authentication.getName()));
     }
 }
