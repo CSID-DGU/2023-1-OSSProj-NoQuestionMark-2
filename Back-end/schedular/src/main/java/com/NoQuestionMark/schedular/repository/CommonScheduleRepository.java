96d0a5ced5f556d8e0c375d695be8cf2cac739bd
shoeone96 <dlwnddnjs96@gmail.com>
2023-05-05 21:41:55 +0900

    feat(repository): 스케쥴을 받아오기 위한 repository 작성

--- Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/repository/CommonScheduleRepository.java
+++ Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/repository/CommonScheduleRepository.java
@@ -1,8 +1,11 @@
 package com.NoQuestionMark.schedular.repository;
 
 import com.NoQuestionMark.schedular.model.entity.CommonScheduleEntity;
+import com.NoQuestionMark.schedular.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommonScheduleRepository extends JpaRepository<CommonScheduleEntity, Long> {
+import java.util.List;
 
+public interface CommonScheduleRepository extends JpaRepository<CommonScheduleEntity, Long> {
+    List<CommonScheduleEntity> findAllByUser(UserEntity user);
 }
