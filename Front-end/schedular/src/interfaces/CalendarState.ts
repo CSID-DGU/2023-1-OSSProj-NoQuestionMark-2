a1ab854469c2075d3d5b6143fb29f296bdeff445
bgs-study <qkrwlgp235@gmail.com>
2023-05-13 15:29:49 +0900

    fix(CalendarState): 함수형 컴폰너트 변경에 따라 불필요한 인터페이스 제거

--- Front-end/schedular/src/interfaces/CalendarState.ts
+++ Front-end/schedular/src/interfaces/CalendarState.ts
@@ -1,21 +1,21 @@
-interface ModalState {
-    personalPost: boolean,
-    subjectPost: boolean,
-    personalRead: boolean,
-    subjectRead: boolean,
-}
+
 interface ModalToggle{
     handleModalToggle: (value: string) => void;
 }
 //scheduleType : task, schedule (개인), 과제,시험,발표 (과목)
-//type : personal, subject
-interface EventSourceInput{title: string, startDate: string, endDate?: string, contents:string, importance?: string, scheduleType: string, type?:string }
+//type : personal(개인), subject(과목)
+interface EventSourceInput{
+    title: string, 
+    startDate: string, 
+    endDate?: string, 
+    contents:string, 
+    importance?: string, 
+    scheduleType: string,
+    subject?:string,
+    type?:string
+}
 
 interface Events extends Array<EventSourceInput> {};
 
-interface CalendarState extends ModalState {
-    events : Events;
-}
 
-
-export type{ModalState, CalendarState, ModalToggle};
+export type{ ModalToggle, EventSourceInput,Events};
