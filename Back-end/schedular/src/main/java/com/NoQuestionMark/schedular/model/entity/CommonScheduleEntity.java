5e09a683fb51e24d7b49a61a6544763092675020
shoeone96 <dlwnddnjs96@gmail.com>
2023-05-15 18:12:29 +0900

    refactor(entity): 스케쥴 반환을 위한 entity 수정

--- Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/model/entity/CommonScheduleEntity.java
+++ Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/model/entity/CommonScheduleEntity.java
@@ -29,6 +29,9 @@ public class CommonScheduleEntity {
     private Importance importance;
     private Month startMonth;
     private Month endMonth;
+    private int startYear;
+    private int endYear;
+    @Enumerated(EnumType.STRING)
     private CommonScheduleType commonScheduleType;
     private boolean complete;
     private Timestamp createdAt;
@@ -56,7 +59,9 @@ public class CommonScheduleEntity {
         this.startDate = requestDto.getStartDate();
         this.endDate = requestDto.getEndDate();
         this.complete = false;
-        this.commonScheduleType = requestDto.getCommonScheduleType();
+        this.commonScheduleType = CommonScheduleType.returnType(requestDto.getCommonScheduleType());
+        this.startYear = requestDto.getStartDate().getYear();
+        this.endYear = requestDto.getEndDate().getYear();
     }
 
     public static CommonScheduleEntity fromCommonScheduleDto(CommonScheduleRequestDto requestDto, UserEntity user){
--- Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/model/entity/CommonScheduleType.java
+++ Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/model/entity/CommonScheduleType.java
@@ -1,5 +1,10 @@
 package com.NoQuestionMark.schedular.model.entity;
 
 public enum CommonScheduleType {
-    TASK, SCHEDULE
+    TASK, SCHEDULE;
+
+    public static CommonScheduleType returnType(String type){
+        if(type.equals("TASK")) return CommonScheduleType.TASK;
+        return SCHEDULE;
+    }
 }
