2cf39186cb457f5e5b4b1ae08046841570d24b6f
shoeone96 <dlwnddnjs96@gmail.com>
2023-05-15 18:11:12 +0900

    feat(controller): 일정 반환을 위한api 작성

--- Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/controller/ScheduleController.java
+++ Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/controller/ScheduleController.java
@@ -2,13 +2,18 @@ package com.NoQuestionMark.schedular.controller;
 
 
 import com.NoQuestionMark.schedular.controller.request.CommonScheduleRequestDto;
-import com.NoQuestionMark.schedular.controller.response.CommonScheduleResponseDto;
+import com.NoQuestionMark.schedular.controller.request.SubjectScheduleRequestDto;
 import com.NoQuestionMark.schedular.controller.response.Response;
+import com.NoQuestionMark.schedular.controller.response.ScheduleResponseDto;
+import com.NoQuestionMark.schedular.controller.response.UserScheduleResponseDto;
 import com.NoQuestionMark.schedular.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Month;
+import java.time.YearMonth;
 import java.util.List;
 
 @RestController
@@ -18,13 +23,15 @@ public class ScheduleController {
 
     private final ScheduleService scheduleService;
     @PostMapping("/common")
-    public Response<Void> writeSchedule(@RequestBody CommonScheduleRequestDto requestDto, Authentication authentication){
+    public Response<Void> registerCommonSchedule(@RequestBody CommonScheduleRequestDto requestDto, Authentication authentication){
         scheduleService.writeCommonSchedule(requestDto, authentication.getName());
         return Response.success();
     }
 
     @GetMapping("/common")
-    public Response<List<CommonScheduleResponseDto>> getSchedule(Authentication authentication){
-        return Response.success(scheduleService.getAllSchedule(authentication.getName()));
+    public Response<ScheduleResponseDto> getSchedule(@RequestParam("month") @DateTimeFormat(pattern = "yyyy-MM") YearMonth yearMonth , Authentication authentication){
+        Month month = yearMonth.getMonth();
+        int year = yearMonth.getYear();
+        return Response.success(scheduleService.getAllSchedule(month, year, authentication.getName()));
     }
 }
