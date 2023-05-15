30ccde54bd7724c483381862b62dd5ef854bc0df
bgs-study <qkrwlgp235@gmail.com>
2023-05-15 21:35:14 +0900

    fix(Atom): isLoginCheck의 디폴트값을 isLogin함수로 할당

--- Front-end/schedular/src/recoil/Atom.ts
+++ Front-end/schedular/src/recoil/Atom.ts
@@ -1,11 +1,13 @@
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
+import {isLogin} from 'utils/utils';
+
 const { persistAtom } = recoilPersist();
 
 // 전역상태를 만든고, export 해준다.
 export const isLoginCheck = atom({
     key: 'isLogin',
-    default: false,
+    default: isLogin(),
     effects_UNSTABLE: [persistAtom]
 });
 
