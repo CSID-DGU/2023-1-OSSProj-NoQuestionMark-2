2ed16ad6990334792b7bb7641d4fd7549aed039b
shoeone96 <dlwnddnjs96@gmail.com>
2023-05-15 18:09:40 +0900

    feat(service): 일정 반환을 위한 서비스 로직

--- Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/service/ScheduleService.java
+++ Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/service/ScheduleService.java
@@ -1,17 +1,28 @@
 package com.NoQuestionMark.schedular.service;
 
 import com.NoQuestionMark.schedular.controller.request.CommonScheduleRequestDto;
+import com.NoQuestionMark.schedular.controller.request.SubjectScheduleRequestDto;
 import com.NoQuestionMark.schedular.controller.response.CommonScheduleResponseDto;
+import com.NoQuestionMark.schedular.controller.response.ScheduleResponseDto;
+import com.NoQuestionMark.schedular.controller.response.SubjectScheduleResponseDto;
+import com.NoQuestionMark.schedular.controller.response.UserScheduleResponseDto;
 import com.NoQuestionMark.schedular.exception.ErrorCode;
 import com.NoQuestionMark.schedular.exception.ScheduleException;
 import com.NoQuestionMark.schedular.model.entity.CommonScheduleEntity;
+import com.NoQuestionMark.schedular.model.entity.SubjectScheduleEntity;
 import com.NoQuestionMark.schedular.model.entity.UserEntity;
+import com.NoQuestionMark.schedular.model.entity.UserType;
 import com.NoQuestionMark.schedular.repository.CommonScheduleRepository;
+import com.NoQuestionMark.schedular.repository.SubjectScheduleRepository;
 import com.NoQuestionMark.schedular.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,6 +32,9 @@ public class ScheduleService {
 
     private final UserRepository userRepository;
     private final CommonScheduleRepository commonScheduleRepository;
+    private final SubjectScheduleRepository subjectScheduleRepository;
+    private final EntityManager em;
+
     public void writeCommonSchedule(CommonScheduleRequestDto requestDto, String schoolNumber) {
         UserEntity user = userRepository
                 .findBySchoolNumber(schoolNumber)
@@ -29,13 +43,32 @@ public class ScheduleService {
         commonScheduleRepository.save(schedule);
     }
 
-    public List<CommonScheduleResponseDto> getAllSchedule(String schoolNumber) {
+    public ScheduleResponseDto getAllSchedule(Month month, int year, String schoolNumber) {
         UserEntity user = userRepository
                 .findBySchoolNumber(schoolNumber)
                 .orElseThrow(() -> new ScheduleException(ErrorCode.USER_NOT_FOUND, String.format("%s 학번을 가진 유자가 없습니다.", schoolNumber)));
-        return commonScheduleRepository.findAllByUser(user)
+        List<CommonScheduleResponseDto> cSchedules = em
+                .createQuery("select c from CommonScheduleEntity c where c.user = :user and(c.startYear = :startYear or c.endYear = : endYear) and (c.startMonth = :startMonth or c.endMonth = :endMonth)", CommonScheduleEntity.class)
+                .setParameter("user", user)
+                .setParameter("startYear", year)
+                .setParameter("endYear", year)
+                .setParameter("startMonth", month)
+                .setParameter("endMonth", month)
+                .getResultList()
                 .stream()
-                .map(CommonScheduleResponseDto::fromCommonScheduleEntity)
+                .map(CommonScheduleResponseDto::fromCommonSchedule)
                 .toList();
+        List<SubjectScheduleResponseDto> sSchedules = em
+                .createQuery("select c from SubjectScheduleEntity c where c.user = :user and(c.startYear = :startYear or c.endYear = : endYear) and (c.startMonth = :startMonth or c.endMonth = :endMonth)", SubjectScheduleEntity.class)
+                .setParameter("user", user)
+                .setParameter("startYear", year)
+                .setParameter("endYear", year)
+                .setParameter("startMonth", month)
+                .setParameter("endMonth", month)
+                .getResultList()
+                .stream()
+                .map(SubjectScheduleResponseDto::fromSubjectSchedule)
+                .toList();
+        return new ScheduleResponseDto(cSchedules, sSchedules);
     }
 }
