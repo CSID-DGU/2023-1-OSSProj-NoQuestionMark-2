b31064b9f99b16effd2cc8ac5bb27c9d27f9c91d
shoeone96 <dlwnddnjs96@gmail.com>
2023-05-15 18:19:22 +0900

    feat(exception): 발생할 수 있는 예외 추가

--- Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/exception/ErrorCode.java
+++ Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/exception/ErrorCode.java
@@ -10,7 +10,9 @@ public enum ErrorCode {
     DUPLICATED_CODE(HttpStatus.CONFLICT, "이미 존재하는 학번 정보입니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "찾으시는 유저가 없습니다."),
-    INVALID_PASSWORD(HttpStatus.CONFLICT, "비밀번호가 일치하지 않습니다.");
+    INVALID_PASSWORD(HttpStatus.CONFLICT, "비밀번호가 일치하지 않습니다."),
+    SUBJECT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 과목을 찾을 수 없습니다."),
+    USER_NOT_AUTHORIZED(HttpStatus.UNAUTHORIZED,"교수만 가능한 작업입니다." );
     private HttpStatus status;
     private String message;
 }
