43b8934a6cfd51a301ea14f32512de6178c161b0
shoeone96 <dlwnddnjs96@gmail.com>
2023-05-19 10:11:13 +0900

    feat(exception): 일정 탐색 과정에서 발생 가능한 예외처리

--- Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/exception/ErrorCode.java
+++ Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/exception/ErrorCode.java
@@ -12,7 +12,8 @@ public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "찾으시는 유저가 없습니다."),
     INVALID_PASSWORD(HttpStatus.CONFLICT, "비밀번호가 일치하지 않습니다."),
     SUBJECT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 과목을 찾을 수 없습니다."),
-    USER_NOT_AUTHORIZED(HttpStatus.UNAUTHORIZED,"교수만 가능한 작업입니다." );
+    USER_NOT_AUTHORIZED(HttpStatus.UNAUTHORIZED,"교수만 가능한 작업입니다." ),
+    SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 일정을 찾을 수 없습니다.");
     private HttpStatus status;
     private String message;
 }
