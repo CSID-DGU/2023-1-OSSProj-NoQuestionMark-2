f044f77653035fe083e617a2c71581077b9a6d5c
shoeone96 <dlwnddnjs96@gmail.com>
2023-05-21 23:15:18 +0900

    feat(entity): 일정 수정을 위한 entity 수정 메서드 작성

--- Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/model/entity/CommonScheduleEntity.java
+++ Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/model/entity/CommonScheduleEntity.java
@@ -1,5 +1,6 @@
 package com.NoQuestionMark.schedular.model.entity;
 
+import com.NoQuestionMark.schedular.controller.request.CommonScheduleFixRequestDto;
 import com.NoQuestionMark.schedular.controller.request.CommonScheduleRequestDto;
 import lombok.Builder;
 import lombok.Getter;
@@ -54,7 +55,7 @@ public class CommonScheduleEntity {
         this.user = user;
         this.startMonth = requestDto.getStartDate().getMonth();
         this.endMonth = requestDto.getEndDate().getMonth();
-        this.importance = requestDto.getImportance();
+        this.importance = Importance.returnType(requestDto.getImportance());
         this.contents = requestDto.getContents();
         this.startDate = requestDto.getStartDate();
         this.endDate = requestDto.getEndDate();
@@ -67,4 +68,19 @@ public class CommonScheduleEntity {
     public static CommonScheduleEntity fromCommonScheduleDto(CommonScheduleRequestDto requestDto, UserEntity user){
         return new CommonScheduleEntity(requestDto, user);
     }
+
+    public void fixSchedule(CommonScheduleFixRequestDto requestDto){
+        this.title = requestDto.getTitle();
+        this.startMonth = requestDto.getStartDate().getMonth();
+        this.endMonth = requestDto.getEndDate().getMonth();
+        this.importance = Importance.returnType(requestDto.getImportance());
+        this.contents = requestDto.getContents();
+        this.startDate = requestDto.getStartDate();
+        this.endDate = requestDto.getEndDate();
+        this.commonScheduleType = CommonScheduleType.returnType(requestDto.getCommonScheduleType());
+        this.startYear = requestDto.getStartDate().getYear();
+        this.endYear = requestDto.getEndDate().getYear();
+    }
+
+
 }
