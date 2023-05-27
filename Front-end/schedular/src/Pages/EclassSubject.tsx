33c2ad26b35dddf5ebdcdc09ea092cd235cd33f7
EunJeongmin <dmswjdals12@naver.com>
2023-05-27 14:34:10 +0900

    feat(EclassSubject): 이클래스 과제등록페이지로 이동하는 등록하기 버튼  추가

--- Front-end/schedular/src/Pages/EclassSubject.tsx
+++ Front-end/schedular/src/Pages/EclassSubject.tsx
@@ -1,14 +1,28 @@
 import EclassMenu from './../Components/EclassMenu';
 import EclassClassName from 'Components/EclassClassName';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledLink = styled(Link)`
+  text-align: center;
+  color: #fff;
+  padding: 9px 18px;
+  border: none;
+  border-radius: 3px;
+  background-color: #228be6;
+  font-size: 13px;
+  cursor: pointer;
+`;
 
 const EclassSubject = () =>{
 
-    return (
-        <>
-            <EclassMenu />
-            <EclassClassName/>
-        </>
-    )
+  return (
+    <>
+      <EclassMenu />
+      <EclassClassName/>
+      <StyledLink to='/eclass/add'>등록하기</StyledLink>
+    </>
+  )
 
 }
 
