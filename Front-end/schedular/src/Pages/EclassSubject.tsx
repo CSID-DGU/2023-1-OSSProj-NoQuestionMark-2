c018c576d4bfa85030254c33396e341f4201c1c3
EunJeongmin <dmswjdals12@naver.com>
2023-05-27 15:23:08 +0900

    feat(EclassSubject): usertype에 따른 과제 등록하기 버튼 유무

--- Front-end/schedular/src/Pages/EclassSubject.tsx
+++ Front-end/schedular/src/Pages/EclassSubject.tsx
@@ -2,8 +2,11 @@ import EclassMenu from './../Components/EclassMenu';
 import EclassClassName from 'Components/EclassClassName';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { useRecoilValue} from 'recoil';
+import { userInfoState } from 'recoil/Atom'
 
 const StyledLink = styled(Link)`
+  margin: 30px;
   text-align: center;
   color: #fff;
   padding: 9px 18px;
@@ -14,13 +17,22 @@ const StyledLink = styled(Link)`
   cursor: pointer;
 `;
 
+const isStudent = (userType:string|null):boolean => {
+  if(userType === "STUDENT") {
+    return true
+  } return false;
+}
+
 const EclassSubject = () =>{
+
+  const userType = useRecoilValue(userInfoState).userType;
+  const STUDENT = isStudent(userType);
 
   return (
     <>
       <EclassMenu />
       <EclassClassName/>
-      <StyledLink to='/eclass/add'>등록하기</StyledLink>
+      { STUDENT ? '' : <StyledLink to='/eclass/add'>과제등록하기</StyledLink>}
     </>
   )
 
