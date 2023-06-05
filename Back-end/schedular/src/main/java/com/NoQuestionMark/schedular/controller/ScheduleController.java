a82261611319c87e5e71e88c9cfe40c9529c7930
shoeone96 <dlwnddnjs96@gmail.com>
2023-06-05 12:18:28 +0900

    refactor(controller): 상속관계 매핑으로 DB 리팩토링으로 인한 Controller 전면 수정

--- Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/controller/ScheduleController.java
+++ Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/controller/ScheduleController.java
@@ -5,6 +5,7 @@ import com.NoQuestionMark.schedular.controller.request.CommonScheduleFixRequestD
 import com.NoQuestionMark.schedular.controller.request.CommonScheduleRequestDto;
 import com.NoQuestionMark.schedular.controller.request.OfficialScheduleRequestDto;
 import com.NoQuestionMark.schedular.controller.request.SubjectScheduleRequestDto;
+import com.NoQuestionMark.schedular.controller.response.OfficialScheduleResponseDto;
 import com.NoQuestionMark.schedular.controller.response.Response;
 import com.NoQuestionMark.schedular.controller.response.ScheduleResponseDto;
 import com.NoQuestionMark.schedular.service.ScheduleService;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.Month;
 import java.time.YearMonth;
+import java.util.List;
 
 @RestController
 @RequestMapping("/schedule")
@@ -31,6 +33,18 @@ public class ScheduleController {
     @PostMapping("/subject")
     public Response<Void> registerSubjectSchedule(@RequestBody SubjectScheduleRequestDto requestDto, Authentication authentication){
         scheduleService.writeSubjectSchedule(requestDto, authentication.getName());
+        return Response.success();
+    }
+
+    @PutMapping("/subject/{scheduleId}")
+    public Response<Void> modifySubjectSchedule(@RequestBody SubjectScheduleRequestDto requestDto, @PathVariable Long scheduleId,Authentication authentication){
+        scheduleService.modifySubjectSchedule(requestDto, scheduleId,authentication.getName());
+        return Response.success();
+    }
+
+    @DeleteMapping("/subject/{scheduleId}")
+    public Response<Void> deleteSubjectSchedule(@PathVariable Long scheduleId,Authentication authentication){
+        scheduleService.deleteSubjectSchedule(scheduleId,authentication.getName());
         return Response.success();
     }
 
@@ -59,6 +73,11 @@ public class ScheduleController {
         return Response.success(scheduleService.getAllSchedule(month, year, authentication.getName()));
     }
 
+    @GetMapping("/official")
+    public Response<List<OfficialScheduleResponseDto>> getSchedule(@RequestParam("subjectName") String subjectName, Authentication authentication){
+        return Response.success(scheduleService.getOfficialSchedules(authentication.getName(), subjectName));
+    }
+
     @DeleteMapping("/common/{scheduleId}")
     public Response<Void> deleteSchedule(@PathVariable Long scheduleId, Authentication authentication){
         scheduleService.deleteSchedule(scheduleId, authentication.getName());
@@ -70,4 +89,9 @@ public class ScheduleController {
         scheduleService.modifySchedule(scheduleId, authentication.getName(), requestDto);
         return Response.success();
     }
+
+//    @GetMapping("/toDoList")
+//    public Response<List<ToDoListResponseDto>> getTodoList(Authentication authentication){
+//        return Response.success(scheduleService.getTodoList(authentication.getName(), subjectName));
+//    }
 }
