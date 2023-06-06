dbac353929c1bcfe229a85185940ee16f30e8c5d
shoeone96 <dlwnddnjs96@gmail.com>
2023-06-06 16:49:23 +0900

    feat(repository): 완료하기 기능을 위한 repository

--- Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/repository/CompleteRepository.java
+++ Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/repository/CompleteRepository.java
@@ -2,9 +2,13 @@ package com.NoQuestionMark.schedular.repository;
 
 
 import com.NoQuestionMark.schedular.model.Complete;
+import com.NoQuestionMark.schedular.model.ScheduleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CompleteRepository extends JpaRepository<Complete, Long> {
+    Optional<Complete> findBySchedule(ScheduleEntity schedule);
 }
