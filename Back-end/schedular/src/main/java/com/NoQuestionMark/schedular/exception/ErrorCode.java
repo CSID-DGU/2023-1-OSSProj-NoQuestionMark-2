c10764aba1e0c6f01ff408a88b277ec6ca33943b
shoeone96 <dlwnddnjs96@gmail.com>
2023-06-06 16:41:24 +0900

    feat(exception): 완료하기 기능 사용 시 발생 가능한 예외 추가

--- Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/exception/ErrorCode.java
+++ Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/exception/ErrorCode.java
@@ -13,7 +13,9 @@ public enum ErrorCode {
     INVALID_PASSWORD(HttpStatus.CONFLICT, "비밀번호가 일치하지 않습니다."),
     SUBJECT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 과목을 찾을 수 없습니다."),
     USER_NOT_AUTHORIZED(HttpStatus.UNAUTHORIZED,"교수만 가능한 작업입니다." ),
-    SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 일정을 찾을 수 없습니다.");
+    SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 일정을 찾을 수 없습니다."),
+    SCHEDULE_TYPE_PROBLEM(HttpStatus.CONFLICT, "스케쥴이 잘못 기록되었습니다."),
+    INVALID_SCHEDULE(HttpStatus.BAD_REQUEST, "해당 일정으로는 요청하신 내용을 처리할 수 없습니다.");
     private HttpStatus status;
     private String message;
 }
