98da53aba2fb0f8b5b01db0f8e83cffc5c93da5a
shoeone96 <dlwnddnjs96@gmail.com>
2023-06-10 14:41:27 +0900

    feat(controller): complete 정보를 끌어오는 로직 변경으로 인해 api 상에서 받아오는 로직 및 전달 방식 변경

--- Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/controller/CompleteController.java
+++ Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/controller/CompleteController.java
@@ -1,11 +1,13 @@
 package com.NoQuestionMark.schedular.controller;
 
+import com.NoQuestionMark.schedular.controller.request.CompleteRequestDto;
 import com.NoQuestionMark.schedular.controller.response.Response;
 import com.NoQuestionMark.schedular.service.CompleteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +16,9 @@ public class CompleteController {
     private final CompleteService completeService;
 
     @PostMapping("/complete/{scheduleId}")
-    public Response<Void> complete(@PathVariable Long scheduleId, Authentication authentication){
-        return Response.success(completeService.complete(scheduleId, authentication.getName()));
+    public Response<Void> complete(@PathVariable Long scheduleId, Authentication authentication, @RequestBody CompleteRequestDto reqeustDto){
+        completeService.complete(scheduleId, authentication.getName(), reqeustDto.getSchedule());
+        return Response.success();
     }
 
 }
