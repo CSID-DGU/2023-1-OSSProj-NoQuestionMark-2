41baf2b886cfd876732a3430caed9419fea18d50
shoeone96 <dlwnddnjs96@gmail.com>
2023-06-10 16:07:52 +0900

    refactor(service): 기존 서비스 로직에서 DB 변경으로 인해 달라진 service 로직 수정

--- Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/service/ScheduleService.java
+++ Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/service/ScheduleService.java
@@ -1,7 +1,6 @@
 package com.NoQuestionMark.schedular.service;
 
 
-
 import com.NoQuestionMark.schedular.controller.request.CommonScheduleFixRequestDto;
 import com.NoQuestionMark.schedular.controller.request.CommonScheduleRequestDto;
 import com.NoQuestionMark.schedular.controller.request.OfficialScheduleRequestDto;
@@ -34,21 +33,14 @@ public class ScheduleService {
     private final SubjectRepository subjectRepository;
     private final UserSubjectRepository userSubjectRepository;
     private final OfficialSubjectRepository officialSubjectRepository;
-    private final CompleteRepository completeRepository;
-
-    public void writeCommonSchedule(CommonScheduleRequestDto requestDto, String schoolNumber) {
-        UserEntity user = userRepository
-                .findBySchoolNumber(schoolNumber)
-                .orElseThrow(() -> new ScheduleException(ErrorCode.USER_NOT_FOUND, String.format("%s 학번을 가진 유자가 없습니다.", schoolNumber)));
-        CommonScheduleEntity schedule = CommonScheduleEntity.fromCommonScheduleDto(requestDto, user);
-        commonScheduleRepository.save(schedule);
-    }
+    private final UserOfficialScheduleRepository userOfficialScheduleRepository;
+    private final ScheduleRepository scheduleRepository;
 
     public ScheduleResponseDto getAllSchedule(Month month, int year, String schoolNumber) {
         UserEntity user = userRepository
                 .findBySchoolNumber(schoolNumber)
                 .orElseThrow(() -> new ScheduleException(ErrorCode.USER_NOT_FOUND, String.format("%s 학번을 가진 유자가 없습니다.", schoolNumber)));
-        List<CommonScheduleResponseDto> cSchedules = em
+        List<CommonScheduleResponseDto> cSchedules = new ArrayList<>(em
                 .createQuery("select c from CommonScheduleEntity c where c.user = :user and(c.startYear = :startYear or c.endYear = : endYear) and (c.startMonth = :startMonth or c.endMonth = :endMonth)", CommonScheduleEntity.class)
                 .setParameter("user", user)
                 .setParameter("startYear", year)
@@ -56,40 +48,38 @@ public class ScheduleService {
                 .setParameter("startMonth", month)
                 .setParameter("endMonth", month)
                 .getResultList()
+                .stream().map(CommonScheduleResponseDto::fromSchedule).toList());
+
+        List<SubjectScheduleResponseDto> sSchedules = new ArrayList<>(em
+                .createQuery("select c from SubjectScheduleEntity c where c.user = :user and(c.startYear = :startYear or c.endYear = : endYear) and (c.startMonth = :startMonth or c.endMonth = :endMonth)", SubjectScheduleEntity.class)
+                .setParameter("user", user)
+                .setParameter("startYear", year)
+                .setParameter("endYear", year)
+                .setParameter("startMonth", month)
+                .setParameter("endMonth", month)
+                .getResultList()
                 .stream()
-                .map(CommonScheduleResponseDto::fromCommonSchedule)
-                .toList();
-        List<UserSubjectsResponseDto> userSubjects = new ArrayList<>(userSubjectRepository
-                .findAllByUser(user)
-                .stream()
-                .map(UserSubjectsResponseDto::fromUserSubject).toList());
-        List<SubjectScheduleResponseDto> sSchedules = new ArrayList<>();
+                .map(SubjectScheduleResponseDto::fromSubjectSchedule)
+                .toList());
+
+        List<UserSubject> userSubjects = userSubjectRepository.findAllByUser(user);
         List<OfficialScheduleResponseDto> oSchedules = new ArrayList<>();
-        for (UserSubjectsResponseDto userSubject : userSubjects) {
-            SubjectEntity subject = subjectRepository.findBySubjectName(userSubject.getSubjectName())
-                    .orElseThrow(() -> new ScheduleException(ErrorCode.SUBJECT_NOT_FOUND, String.format("%s에 해당하는 과목이 존재하지 않습니다.", userSubject.getSubjectName())));
-            sSchedules.addAll(em
-                    .createQuery("select c from SubjectScheduleEntity c where c.subject = :subject and(c.startYear = :startYear or c.endYear = : endYear) and (c.startMonth = :startMonth or c.endMonth = :endMonth)", SubjectScheduleEntity.class)
-                    .setParameter("subject", subject)
-                    .setParameter("startYear", year)
-                    .setParameter("endYear", year)
-                    .setParameter("startMonth", month)
-                    .setParameter("endMonth", month)
-                    .getResultList()
-                    .stream()
-                    .map(SubjectScheduleResponseDto::fromSubjectSchedule)
-                    .toList());
-            oSchedules.addAll(em
+        for (UserSubject userSubject : userSubjects) {
+            List<OfficialSubjectScheduleEntity> officialSchedules = em
                     .createQuery("select c from OfficialSubjectScheduleEntity c where c.subject = :subject and(c.startYear = :startYear or c.endYear = : endYear) and (c.startMonth = :startMonth or c.endMonth = :endMonth)", OfficialSubjectScheduleEntity.class)
-                    .setParameter("subject", subject)
+                    .setParameter("subject", userSubject.getSubject())
                     .setParameter("startYear", year)
                     .setParameter("endYear", year)
                     .setParameter("startMonth", month)
                     .setParameter("endMonth", month)
-                    .getResultList()
-                    .stream()
-                    .map(OfficialScheduleResponseDto::officialScheduleResponseDto)
-                    .toList());
+                    .getResultList();
+            for (OfficialSubjectScheduleEntity schedule : officialSchedules) {
+                String complete = userOfficialScheduleRepository
+                        .findByScheduleAndUser(schedule, user)
+                        .orElseThrow(() -> new ScheduleException(ErrorCode.SCHEDULE_TYPE_PROBLEM))
+                        .getComplete().name();
+                oSchedules.add(OfficialScheduleResponseDto.fromOfficialSchedule(schedule, complete));
+            }
         }
 
         return new ScheduleResponseDto(cSchedules, sSchedules, oSchedules);
@@ -102,14 +92,35 @@ public class ScheduleService {
         SubjectEntity subject = subjectRepository
                 .findBySubjectName(requestDto.getClassName())
                 .orElseThrow(() -> new ScheduleException(ErrorCode.SUBJECT_NOT_FOUND, String.format("%s 과목이 존재하지 않습니다.", requestDto.getClassName())));
-        SubjectScheduleEntity subjectSchedule = SubjectScheduleEntity.fromSubjectScheduleDto(requestDto, user, subject);
+        SubjectScheduleEntity subjectSchedule = SubjectScheduleEntity.fromSubjectScheduleDto(requestDto, user, subject, requestDto.getScheduleType());
         subjectScheduleRepository.save(subjectSchedule);
-        if (subjectSchedule.getScheduleType() == ScheduleType.TASK) {
-            completeRepository.save(Complete.newAssignmentFromOfficial(user, subjectSchedule));
-        }
     }
 
-    public void deleteSchedule(Long scheduleId, String schoolNumber) {
+    public void modifySubjectSchedule(SubjectScheduleRequestDto requestDto, Long scheduleId, String schoolNumber) {
+        UserEntity user = userRepository
+                .findBySchoolNumber(schoolNumber)
+                .orElseThrow(() -> new ScheduleException(ErrorCode.USER_NOT_FOUND, String.format("%s 학번을 가진 유자가 없습니다.", schoolNumber)));
+        SubjectScheduleEntity subjectSchedule = subjectScheduleRepository
+                .findByUserAndId(user, scheduleId)
+                .orElseThrow(() -> new ScheduleException(ErrorCode.SCHEDULE_NOT_FOUND, String.format("%s가 작성한 %d 일정을 확인할 수 없습니다.", schoolNumber, scheduleId)));
+        SubjectEntity subject = subjectRepository.findBySubjectName(requestDto.getClassName()).orElseThrow(() -> new ScheduleException(ErrorCode.SUBJECT_NOT_FOUND));
+        if (subjectSchedule.getComplete().name().equals("TRUE")) {
+            subjectSchedule.modifySchedule(requestDto, subject, "done");
+            return;
+        }
+        subjectSchedule.modifySchedule(requestDto, subject, requestDto.getScheduleType());
+    }
+
+
+    public void writeCommonSchedule(CommonScheduleRequestDto requestDto, String schoolNumber) {
+        UserEntity user = userRepository
+                .findBySchoolNumber(schoolNumber)
+                .orElseThrow(() -> new ScheduleException(ErrorCode.USER_NOT_FOUND, String.format("%s 학번을 가진 유자가 없습니다.", schoolNumber)));
+        CommonScheduleEntity schedule = CommonScheduleEntity.fromCommonScheduleDto(requestDto, user);
+        commonScheduleRepository.save(schedule);
+    }
+
+    public void deleteCommonSchedule(Long scheduleId, String schoolNumber) {
         UserEntity user = userRepository
                 .findBySchoolNumber(schoolNumber)
                 .orElseThrow(() -> new ScheduleException(ErrorCode.USER_NOT_FOUND, String.format("%s 학번을 가진 유자가 없습니다.", schoolNumber)));
@@ -119,18 +130,18 @@ public class ScheduleService {
         commonScheduleRepository.delete(commonSchedule);
     }
 
-    public void modifySchedule(Long scheduleId, String schoolNumber, CommonScheduleFixRequestDto requestDto) {
+    public void modifyCommonSchedule(Long scheduleId, String schoolNumber, CommonScheduleFixRequestDto requestDto) {
         UserEntity user = userRepository
                 .findBySchoolNumber(schoolNumber)
                 .orElseThrow(() -> new ScheduleException(ErrorCode.USER_NOT_FOUND, String.format("%s 학번을 가진 유자가 없습니다.", schoolNumber)));
         CommonScheduleEntity commonSchedule = commonScheduleRepository
                 .findByIdAndUser(scheduleId, user)
                 .orElseThrow(() -> new ScheduleException(ErrorCode.SCHEDULE_NOT_FOUND, String.format("%s 유저가 작성한 %d 일정을 찾을 수 없습니다.", schoolNumber, scheduleId)));
-        ScheduleType beforeType = commonSchedule.getScheduleType();
-        commonSchedule.fixSchedule(requestDto);
-        if (beforeType == ScheduleType.SCHEDULE && commonSchedule.getScheduleType() == ScheduleType.TASK) {
-            completeRepository.save(Complete.newAssignmentFromOfficial(user, commonSchedule));
+        if (commonSchedule.getComplete().name().equals("TRUE")) {
+            commonSchedule.modifySchedule(requestDto, "done");
+            return;
         }
+        commonSchedule.modifySchedule(requestDto, requestDto.getScheduleType());
     }
 
     public void writeOfficialSchedule(OfficialScheduleRequestDto requestDto, String schoolNumber) {
@@ -146,16 +157,14 @@ public class ScheduleService {
                 .findByUserAndSubject(user, subject)
                 .orElseThrow(() -> new ScheduleException(ErrorCode.USER_NOT_AUTHORIZED, String.format("%s 과목에 대해 일정을 작성할 권한이 없습니다.", requestDto.getClassName())));
         OfficialSubjectScheduleEntity subjectSchedule = OfficialSubjectScheduleEntity.fromOfficialScheduleDto(requestDto, subject, user);
-        ScheduleEntity schedule = officialSubjectRepository.saveAndFlush(subjectSchedule);
-        if (subjectSchedule.getSubjectScheduleType() == SubjectScheduleType.ASSIGNMENT) {
-            List<UserEntity> users = userSubjectRepository
-                    .findAllBySubject(subject)
-                    .stream()
-                    .map(UserSubject::getUser)
-                    .toList();
-            for (UserEntity listener : users) {
-                completeRepository.save(Complete.newAssignmentFromOfficial(listener, schedule));
-            }
+        officialSubjectRepository.saveAndFlush(subjectSchedule);
+        List<UserEntity> users = userSubjectRepository
+                .findAllBySubject(subject)
+                .stream()
+                .map(UserSubject::getUser)
+                .toList();
+        for (UserEntity listener : users) {
+            userOfficialScheduleRepository.save(UserOfficialScheduleEntity.newAssignmentFromOfficial(listener, subjectSchedule));
         }
     }
 
@@ -174,18 +183,20 @@ public class ScheduleService {
         OfficialSubjectScheduleEntity schedule = officialSubjectRepository
                 .findById(scheduleId)
                 .orElseThrow(() -> new ScheduleException(ErrorCode.SCHEDULE_NOT_FOUND, String.format("%d 일정을 확인할 수 없습니다.", scheduleId)));
-        schedule.scheduleFix(requestDto);
-        if (schedule.getSubjectScheduleType() == SubjectScheduleType.ASSIGNMENT) {
+        if (!(schedule.getSubjectScheduleType() == SubjectScheduleType.ASSIGNMENT) && requestDto.getSubjectScheduleType().equals("ASSIGNMENT")) {
             List<UserEntity> users = userSubjectRepository
                     .findAllBySubject(subject)
                     .stream()
                     .map(UserSubject::getUser)
                     .toList();
             for (UserEntity listener : users) {
-                completeRepository.save(Complete.newAssignmentFromOfficial(listener, schedule));
+                userOfficialScheduleRepository.save(UserOfficialScheduleEntity.newAssignmentFromOfficial(listener, schedule));
             }
         }
-
+        if ((schedule.getSubjectScheduleType() == SubjectScheduleType.ASSIGNMENT) && !requestDto.getSubjectScheduleType().equals("ASSIGNMENT")) {
+            userOfficialScheduleRepository.deleteAllBySchedule(schedule);
+        }
+        schedule.scheduleFix(requestDto);
     }
 
     public void deleteOfficialSchedule(Long scheduleId, String schoolNumber) {
@@ -200,39 +211,8 @@ public class ScheduleService {
         officialSubjectRepository.delete(schedule);
     }
 
-    public void modifySubjectSchedule(SubjectScheduleRequestDto requestDto, Long scheduleId, String schoolNumber) {
-        UserEntity user = userRepository
-                .findBySchoolNumber(schoolNumber)
-                .orElseThrow(() -> new ScheduleException(ErrorCode.USER_NOT_FOUND, String.format("%s 학번을 가진 유자가 없습니다.", schoolNumber)));
-        SubjectScheduleEntity subjectSchedule = subjectScheduleRepository
-                .findByUserAndId(user, scheduleId)
-                .orElseThrow(() -> new ScheduleException(ErrorCode.SCHEDULE_NOT_FOUND, String.format("%s가 작성한 %d 일정을 확인할 수 없습니다.", schoolNumber, scheduleId)));
-        SubjectEntity subject = subjectRepository.findBySubjectName(requestDto.getClassName()).orElseThrow(() -> new ScheduleException(ErrorCode.SUBJECT_NOT_FOUND));
-        SubjectScheduleType beforeType = subjectSchedule.getSubjectScheduleType();
-        subjectSchedule.modifySchedule(requestDto, subject);
-        if (!(beforeType == SubjectScheduleType.ASSIGNMENT) && subjectSchedule.getSubjectScheduleType() == SubjectScheduleType.ASSIGNMENT) {
-            List<UserEntity> users = userSubjectRepository
-                    .findAllBySubject(subject)
-                    .stream()
-                    .map(UserSubject::getUser)
-                    .toList();
-            for (UserEntity listener : users) {
-                completeRepository.save(Complete.newAssignmentFromOfficial(listener, subjectSchedule));
-            }
-        }
-    }
 
-    public void deleteSubjectSchedule(Long scheduleId, String schoolNumber) {
-        UserEntity user = userRepository
-                .findBySchoolNumber(schoolNumber)
-                .orElseThrow(() -> new ScheduleException(ErrorCode.USER_NOT_FOUND, String.format("%s 학번을 가진 유자가 없습니다.", schoolNumber)));
-        SubjectScheduleEntity subjectSchedule = subjectScheduleRepository
-                .findByUserAndId(user, scheduleId)
-                .orElseThrow(() -> new ScheduleException(ErrorCode.SCHEDULE_NOT_FOUND, String.format("%s가 작성한 %d 일정을 확인할 수 없습니다.", schoolNumber, scheduleId)));
-        subjectScheduleRepository.delete(subjectSchedule);
-    }
-
-    public List<OfficialScheduleResponseDto> getOfficialSchedules(String schoolNumber, String subjectName) {
+    public List<EclassOfficialScheduleResponseDto> getOfficialSchedules(String schoolNumber, String subjectName) {
         UserEntity user = userRepository
                 .findBySchoolNumber(schoolNumber)
                 .orElseThrow(() -> new ScheduleException(ErrorCode.USER_NOT_FOUND, String.format("%s 학번을 가진 유자가 없습니다.", schoolNumber)));
@@ -243,7 +223,7 @@ public class ScheduleService {
         return new ArrayList<>(officialSubjectRepository
                 .findAllBySubject(subject)
                 .stream()
-                .map(OfficialScheduleResponseDto::officialScheduleResponseDto)
+                .map(EclassOfficialScheduleResponseDto::officialScheduleResponseDto)
                 .toList());
     }
 
@@ -252,12 +232,23 @@ public class ScheduleService {
                 .findBySchoolNumber(schoolNumber)
                 .orElseThrow(() -> new ScheduleException(ErrorCode.USER_NOT_FOUND, String.format("%s 학번을 가진 유자가 없습니다.", schoolNumber)));
         List<UserSubject> userSubject = userSubjectRepository.findAllByUser(user);
-        List<ToDoListResponseDto> toDoList = new ArrayList<>(commonScheduleRepository.findCommonToDoList(user, LocalDateTime.now()).stream().map(ToDoListResponseDto::fromCommon).toList());
-        toDoList.addAll(subjectScheduleRepository.findSubjectToDoList(user, LocalDateTime.now()).stream().map(ToDoListResponseDto::fromSubject).toList());
-        for(UserSubject subject : userSubject){
-            toDoList.addAll(officialSubjectRepository.findOfficialToDoList(subject.getSubject(), LocalDateTime.now()).stream().map(ToDoListResponseDto::fromOfficial).toList());
+        List<ToDoListResponseDto> toDoList = new ArrayList<>(
+                commonScheduleRepository.findCommonToDoList(user, LocalDateTime.now())
+                        .stream()
+                        .map(ToDoListResponseDto::fromCommon)
+                        .toList());
+        toDoList.addAll(subjectScheduleRepository
+                .findSubjectToDoList(user, LocalDateTime.now())
+                .stream()
+                .map(ToDoListResponseDto::fromSubject).toList());
+        for (UserSubject subject : userSubject) {
+            List<OfficialSubjectScheduleEntity> officialToDoList = officialSubjectRepository.findOfficialToDoList(subject.getSubject(), LocalDateTime.now());
+            for (OfficialSubjectScheduleEntity todolist : officialToDoList) {
+                String complete = userOfficialScheduleRepository.findByScheduleAndUser(todolist, user).orElseThrow(() -> new ScheduleException(ErrorCode.SCHEDULE_TYPE_PROBLEM)).getComplete().name();
+                toDoList.add(ToDoListResponseDto.fromOfficial(todolist, complete));
+            }
         }
-        toDoList.sort(Comparator.comparing(ToDoListResponseDto::getDDay));
+        toDoList.sort(Comparator.comparing(ToDoListResponseDto::getDDay).reversed());
         return toDoList;
     }
 }
