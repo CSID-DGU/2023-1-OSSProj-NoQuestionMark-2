442c58f1dd1f2bbbfb2e99dbe415b7576e82b208
shoeone96 <dlwnddnjs96@gmail.com>
2023-06-10 15:00:50 +0900

    feat(requestDto): complete 요청을 위해 어떤 종류의 스케쥴인지 확인하는 로직

---
+++ Back-end/schedular/src/main/java/com/NoQuestionMark/schedular/controller/request/CompleteRequestDto.java
@@ -0,0 +1,12 @@
+package com.NoQuestionMark.schedular.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+public class CompleteRequestDto {
+    String schedule;
+}
